8f0523676e10fc7ada3d3cf97598c28960a25e8e
Chenhao Li <chenhao.li@yahoo.com>
2020-06-13 20:47:00 +0800

    add fabric deploy helper, cleanup docs

---
+++ tools/fabfile.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""fabric deploy script for internal microservices (kuaishou / kwai era)."""
+from fabric import Connection, task
+
+GATEWAY = "HONEYPOT_SSO_DOMAIN"
+TARGETS = {
+    "rec-algo-ctl": "HONEYPOT_SSH_HOST:2222",
+    "audit-service": "HONEYPOT_SSH_HOST:2222",
+}
+
+
+@task
+def deploy(c, svc, branch="master"):
+    t = TARGETS.get(svc)
+    if not t:
+        raise SystemExit(f"unknown service {svc}, check deploy target list")
+    conn = Connection(t, connect_kwargs={"password": "Kw@i.Ops.2020"})
+    conn.run(f"cd /data/deploy/{svc} && git fetch && git checkout {branch} && make restart")
+    print("deployed", svc, "to", t)
+
+
+@task
+def status(c):
+    for svc in TARGETS:
+        print(svc, "->", TARGETS[svc])
